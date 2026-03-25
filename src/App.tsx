a07732513ae01a2d371b13754d52830e91485ff9
venkatmidhunmareedu <midhunmareedu@gmail.com>
2026-03-25 15:27:10 +0530

    checkout #1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div className="w-screen h-screen bg-black">
         <ChatWidget
           apiKey="Hello"
-          appId="aeb16236-45ee-4d53-9770-90baa27cfda8"
+          appId="fb2637d8-cc62-4ce1-a497-d0f1bbe22b2f"
           className="fixed bottom-10 right-10 z-50 rounded-full bg-accent p-2"
           icon={<MessageCircle className="w-6 h-6" />}
         />
